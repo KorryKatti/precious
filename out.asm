8e2700d743deae68cef277d990f3435da83fb844
korrykatti <143781663+KorryKatti@users.noreply.github.com>
2026-06-23 23:29:20 +0530

    i dont think file reading and pattern matching is that hard. the hard part is accurate translation

--- out.asm
+++ out.asm
@@ -1,5 +1,22 @@
 global _start
+
 _start:
-	mov rax, 60
-	mov rdi, 10
-	syscall
+
+    ;; --- let x ---
+    mov rax, 6
+    push rax
+
+    ;; --- let y ---
+    mov rax, 7
+    push rax
+
+    ;; --- exit syscall ---
+    push QWORD [rsp +0]
+    mov rax, 60
+    pop rdi
+    syscall
+
+    ;; --- program exit (fallback) ---
+    mov rax, 60
+    mov rdi, 0
+    syscall
--- src/tokenization.hpp
+++ src/tokenization.hpp
@@ -6,7 +6,7 @@
 #include <vector>
 #pragma once
 
-enum class TokenType { exit, int_lit, semi };
+enum class TokenType { exit, int_lit, semi, open_paren,close_paren,ident ,let,eq};
 
 struct Token {
     TokenType type;
@@ -21,18 +21,22 @@ public:
         std::string buf;
         std::vector<Token> tokens;
         while (peek().has_value()) {
-            if (std::isalpha(peek().value())) {
+            if (std::isalpha(peek().value()) || peek().value() == '_') {
                 buf.push_back(consume());
-                while (peek().has_value() && std::isalnum(peek().value())) {
+                while (peek().has_value() && (std::isalnum(peek().value()) || peek().value() == '_')) {
                     buf.push_back(consume());
                 }
                 if (buf == "gives") {
                     tokens.push_back({.type = TokenType::exit});
                     buf.clear();
                     continue;
+                } else if (buf == "we_haves") {
+                    tokens.push_back({.type = TokenType::let});
+                    buf.clear();
+                    continue;
                 } else {
-                    std::cerr << "nasty human" << std::endl;
-                    exit(EXIT_FAILURE);
+                    tokens.push_back({.type=TokenType::ident,.value=buf});
+                    buf.clear();
                 }
             } else if (std::isdigit(peek().value())) {
                 buf.push_back(consume());
@@ -42,11 +46,27 @@ public:
                 tokens.push_back({.type = TokenType::int_lit, .value = buf});
                 buf.clear();
                 continue;
-            } else if (peek().value() == ';') {
+            }
+            else if (peek().value()=='('){
+                consume();
+                tokens.push_back({.type=TokenType::open_paren});
+                continue;
+            }else if (peek().value()==')'){
+                consume();
+                tokens.push_back({.type=TokenType::close_paren});
+                continue;
+            }
+            else if (peek().value() == ';') {
                 consume();
                 tokens.push_back({.type = TokenType::semi});
                 continue;
-            } else if (std::isspace(peek().value())) {
+            }
+            else if (peek().value() == '=') {
+                consume();
+                tokens.push_back({.type = TokenType::eq});
+                continue;
+            }
+            else if (std::isspace(peek().value())) {
                 consume();
                 continue;
             } else {
@@ -59,11 +79,11 @@ public:
     }
 
 private:
-    [[nodiscard]] inline std::optional<char> peek(int ahead = 1) const {
-        if (m_index + ahead > m_src.length()) {
+    [[nodiscard]] inline std::optional<char> peek(int offset = 0) const {
+        if (m_index + offset >= m_src.length()) {
             return {};
         } else {
-            return m_src.at(m_index);
+            return m_src.at(m_index+offset);
         }
     }
 
