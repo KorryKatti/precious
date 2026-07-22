114f53b5423602e0dd2ec873fcf98ed9b37437c2
korrykatti <143781663+KorryKatti@users.noreply.github.com>
2026-07-22 23:13:08 +0530

    brothe5r why no tiki tiki phonk

--- out.c
+++ out.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const char* greet();
 
 int main() {
-    printf("%s\n", greet());
+    long digits[3] = {1, 2, 3};
+    long n = 3;
+    long i = n - 1;
+    while (i >= 0) {
+    if (digits[i] < 9) {
+    digits[i] = digits[i] + 1;
+    long idx = 0;
+    while (idx < n) {
+    printf("%ld\n", digits[idx]);
+    idx = idx + 1;
 }
-
-const char* greet()
-{
-    return "hi";
+    return 1;
+}
+    digits[i] = 0;
+    i = i - 1;
+}
+    printf("%ld\n", 1);
+    long idx = 0;
+    while (idx < n) {
+    printf("%ld\n", digits[idx]);
+    idx = idx + 1;
+}
+    return 1;
 }
 
